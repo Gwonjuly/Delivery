7081350518d5447c28d8bfd58e4f7036ba16f77c
권주리 <rnjswnfl99@gmail.com>
2024-03-19 00:01:22 +0900

    login: session 셍성 (세션 <-(쿠키/헤더/세션))

--- cookie/src/main/java/com/example/cookie/controller/AccountApiController.java
+++ cookie/src/main/java/com/example/cookie/controller/AccountApiController.java
@@ -3,6 +3,7 @@ package com.example.cookie.controller;
 import com.example.cookie.model.LoginRequest;
 import com.example.cookie.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,11 @@ public class AccountApiController {
 
     private final UserService userService;
     @PostMapping("/login")
-    public String login(@RequestBody  LoginRequest loginRequest, HttpServletResponse httpServletResponse){
+    public String login(
+            @RequestBody  LoginRequest loginRequest,
+            HttpServletResponse httpServletResponse,
+            HttpSession httpSession//JSESSIONID 생성
+    ){
         return userService.login(loginRequest, httpServletResponse);
     }
 }
