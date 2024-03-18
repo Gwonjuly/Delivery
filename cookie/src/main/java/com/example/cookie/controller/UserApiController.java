5b5d655377386cb945cd7e8486e8ce0a87f47261
권주리 <rnjswnfl99@gmail.com>
2024-03-18 21:17:18 +0900

    login 후, 쿠키 값 확인 및 보안 처리

---
+++ cookie/src/main/java/com/example/cookie/controller/UserApiController.java
@@ -0,0 +1,38 @@
+package com.example.cookie.controller;
+
+
+import com.example.cookie.db.UserRepository;
+import com.example.cookie.model.UserDto;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+@Slf4j
+public class UserApiController {
+
+    private final UserRepository userRepository;//controller <- repository..? service 안거침
+
+    @GetMapping("/me")//pathvariable (x)
+    public UserDto me(HttpServletRequest httpServletRequest,
+                      @CookieValue(name = "authorization-cookie",required = false) String authorizationCookie//default=true, false로 하여금 쿠기가 없을 때 에러 발생하도록 함
+    ){//쿠기 값 확인(받음)
+        log.info("authorizationCooke:{}",authorizationCookie);//쿠키 값(value) 확인 -> 491a56ce-2df7-45f2-95bf-5ec5775f31fb
+        var optionalUserDto=userRepository.findById(authorizationCookie);
+        return optionalUserDto.get();//-> {"id":"491a56ce-2df7-45f2-95bf-5ec5775f31fb","name":"홍길동","password":"1234"}
+        /*var cookies=httpServletRequest.getCookies();
+        if(cookies !=null){
+            for(Cookie cookie:cookies){
+                log.info("key:{}, value:{}",cookie.getName(), cookie.getValue());//쿠키는 key:value 한 쌍, key=authorization-cookie
+            }
+        }*/
+    }
+}
--- cookie/src/main/java/com/example/cookie/service/UserService.java
+++ cookie/src/main/java/com/example/cookie/service/UserService.java
@@ -14,10 +14,10 @@ public class UserService {
     private final UserRepository userRepository;
     //login logic
     public void login(LoginRequest loginRequest, HttpServletResponse httpServletResponse){
-        var id=loginRequest.getId();
-        var pw=loginRequest.getPassword();
+        var id=loginRequest.getId();//이름
+        var pw=loginRequest.getPassword();//비밀번호
 
-        var optionalUser=userRepository.findByName(id);//login 시, user의 id, pw 입력했기에 id를 인자로 함
+        var optionalUser=userRepository.findByName(id);//login 시, user의 id(이름), pw 입력했기에 id(이름)를 인자로 함
 
         if(optionalUser.isPresent()){
             var userDto=optionalUser.get();
@@ -26,6 +26,8 @@ public class UserService {
                 var cookie=new Cookie("authorization-cookie",userDto.getId());
                 cookie.setDomain("localhost");//ex) naver.com
                 cookie.setPath("/");//모든 경로 root에서  사용 가능
+                cookie.setHttpOnly(true);//쿠키 접근 막음: 자바스크립트에서 해당 값(쿠키)를 읽을 수 없도록 보안 처리함 즉, http 통신할 때만 사용할 수 있음
+                cookie.setSecure(true);//http 에서만 사용 가능 즉, 서버가 http로 사용해야만 클라이언트가 사용할 수 있음, API 통신이 HTTP가 아닌 HTTPS에서만 해당 쿠키 전송하도록 설정
                 cookie.setMaxAge(-1);//session처럼 연결된 동안에만 사용 가능
                 httpServletResponse.addCookie(cookie);//컨트롤러에서 넘어온 httpresponse에 해당 쿠기를 추가
             }
